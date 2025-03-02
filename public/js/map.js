0193a0e6555b0308737e60445b9973340d85373e
Omar Pastrana <omar.pastrana@gmail.com>
2025-03-02 07:49:40 -0600

    adding in the map

---
+++ public/js/map.js
@@ -0,0 +1,7 @@
+//? this will run on the client side
+
+// https://www.mapbox.com/ -> instead trying to use leafletjs.com
+// https://leafletjs.com/examples/quick-start/
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+console.log('locations: ', locations);
